23596b8fef64781c7a1cc5bf371080f6e410a36c
ZuziaSzukiewicz <zizixi2005@gmail.com>
2025-01-13 22:30:12 +0100

    Strefy poszczególnych pól i menu.
    Ustawienie i zdefiniowanie poszczególnych pól i menu. Dodatkowo dodaję ustawienia czcionki.

--- main.py
+++ main.py
@@ -42,3 +42,121 @@ except Exception as e:
     print(f"Wczytanie obrazu nie powiodło się :(: {e}")
     pygame.quit()
     sys.exit()
+    
+#fonty
+font_path = 'czcionka.ttf'
+font1 = pygame.font.SysFont(None, 35)
+WHITE = (255, 255, 255)
+
+#przyciskowe strefy menu
+graj_prostokat = pygame.prostokat(253, 270, 390, 100)
+pc_play_prostokat = pygame.prostokat(253, 436, 390, 100)
+quit_prostokat = pygame.prostokat(253, 591, 390, 100)
+
+---------------------------
+STREFY POSZCZEGÓLNYCH PÓL
+---------------------------
+Sub-plansza 1 (górny-lewy róg)
+Sub-plansza 1: 3 kolumny i 3 wiersze.
+# ---------------------------
+# STREFY POSZCZEGÓLNYCH PÓL
+# ---------------------------
+
+# Sub-plansza 1
+s1p1 = pygame.Rect(57, 47, 65, 65)
+s1p2 = pygame.Rect(127, 47, 65, 65)
+s1p3 = pygame.Rect(197, 47, 65, 65)
+s1p4 = pygame.Rect(57, 119, 65, 65)
+s1p5 = pygame.Rect(127, 119, 65, 65)
+s1p6 = pygame.Rect(197, 119, 65, 65)
+s1p7 = pygame.Rect(57, 193, 65, 65)
+s1p8 = pygame.Rect(127, 193, 65, 65)
+s1p9 = pygame.Rect(197, 193, 65, 65)
+
+# Sub-plansza 2
+s2p1 = pygame.Rect(350, 47, 65, 65)
+s2p2 = pygame.Rect(422, 47, 65, 65)
+s2p3 = pygame.Rect(493, 47, 65, 65)
+s2p4 = pygame.Rect(350, 119, 65, 65)
+s2p5 = pygame.Rect(422, 119, 65, 65)
+s2p6 = pygame.Rect(493, 119, 65, 65)
+s2p7 = pygame.Rect(350, 193, 65, 65)
+s2p8 = pygame.Rect(422, 193, 65, 65)
+s2p9 = pygame.Rect(493, 193, 65, 65)
+
+# Sub-plansza 3
+s3p1 = pygame.Rect(635, 47, 65, 65)
+s3p2 = pygame.Rect(705, 47, 65, 65)
+s3p3 = pygame.Rect(777, 47, 65, 65)
+s3p4 = pygame.Rect(635, 119, 65, 65)
+s3p5 = pygame.Rect(705, 119, 65, 65)
+s3p6 = pygame.Rect(777, 119, 65, 65)
+s3p7 = pygame.Rect(635, 193, 65, 65)
+s3p8 = pygame.Rect(705, 193, 65, 65)
+s3p9 = pygame.Rect(777, 193, 65, 65)
+
+# Sub-plansza 4
+s4p1 = pygame.Rect(57, 337, 65, 65)
+s4p2 = pygame.Rect(127, 337, 65, 65)
+s4p3 = pygame.Rect(197, 337, 65, 65)
+s4p4 = pygame.Rect(57, 408, 65, 65)
+s4p5 = pygame.Rect(127, 408, 65, 65)
+s4p6 = pygame.Rect(197, 408, 65, 65)
+s4p7 = pygame.Rect(57, 485, 65, 65)
+s4p8 = pygame.Rect(127, 485, 65, 65)
+s4p9 = pygame.Rect(197, 485, 65, 65)
+
+# Sub-plansza 5
+s5p1 = pygame.Rect(350, 337, 65, 65)
+s5p2 = pygame.Rect(422, 337, 65, 65)
+s5p3 = pygame.Rect(493, 337, 65, 65)
+s5p4 = pygame.Rect(350, 408, 65, 65)
+s5p5 = pygame.Rect(422, 408, 65, 65)
+s5p6 = pygame.Rect(493, 408, 65, 65)
+s5p7 = pygame.Rect(350, 485, 65, 65)
+s5p8 = pygame.Rect(422, 485, 65, 65)
+s5p9 = pygame.Rect(493, 485, 65, 65)
+
+# Sub-plansza 6
+s6p1 = pygame.Rect(635, 337, 65, 65)
+s6p2 = pygame.Rect(705, 337, 65, 65)
+s6p3 = pygame.Rect(777, 337, 65, 65)
+s6p4 = pygame.Rect(635, 408, 65, 65)
+s6p5 = pygame.Rect(705, 408, 65, 65)
+s6p6 = pygame.Rect(777, 408, 65, 65)
+s6p7 = pygame.Rect(635, 485, 65, 65)
+s6p8 = pygame.Rect(705, 485, 65, 65)
+s6p9 = pygame.Rect(777, 485, 65, 65)
+
+# Sub-plansza 7
+s7p1 = pygame.Rect(57, 627, 65, 65)
+s7p2 = pygame.Rect(127, 627, 65, 65)
+s7p3 = pygame.Rect(197, 627, 65, 65)
+s7p4 = pygame.Rect(57, 700, 65, 65)
+s7p5 = pygame.Rect(127, 700, 65, 65)
+s7p6 = pygame.Rect(197, 700, 65, 65)
+s7p7 = pygame.Rect(57, 777, 65, 65)
+s7p8 = pygame.Rect(127, 777, 65, 65)
+s7p9 = pygame.Rect(197, 777, 65, 65)
+
+# Sub-plansza 8
+s8p1 = pygame.Rect(350, 627, 65, 65)
+s8p2 = pygame.Rect(422, 627, 65, 65)
+s8p3 = pygame.Rect(493, 627, 65, 65)
+s8p4 = pygame.Rect(350, 700, 65, 65)
+s8p5 = pygame.Rect(422, 700, 65, 65)
+s8p6 = pygame.Rect(493, 700, 65, 65)
+s8p7 = pygame.Rect(350, 777, 65, 65)
+s8p8 = pygame.Rect(422, 777, 65, 65)
+s8p9 = pygame.Rect(493, 777, 65, 65)
+
+# Sub-plansza 9
+s9p1 = pygame.Rect(635, 627, 65, 65)
+s9p2 = pygame.Rect(705, 627, 65, 65)
+s9p3 = pygame.Rect(777, 627, 65, 65)
+s9p4 = pygame.Rect(635, 700, 65, 65)
+s9p5 = pygame.Rect(705, 700, 65, 65)
+s9p6 = pygame.Rect(777, 700, 65, 65)
+s9p7 = pygame.Rect(635, 777, 65, 65)
+s9p8 = pygame.Rect(705, 777, 65, 65)
+s9p9 = pygame.Rect(777, 777, 65, 65)
